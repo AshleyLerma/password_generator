7f2860fc473145a5014825829eb71fe6ac6feeca
Ashley Lerma <lerma.scout@gmail.com>
2020-05-01 11:06:08 -0500

    added pseudo code steps to js file for overall needs

---
+++ assets/index.js
@@ -0,0 +1,40 @@
+/*
+Define Variables 
+    user variables
+        var passLength
+        var confirmNum
+        var confirmLowAlpha
+        var confirmUpAlpha
+        var confirmChar
+
+    computer variables
+        var numbers = [number]
+        var alpha = [lower alpha ]
+        var characters = [special characters]
+        var alpha2 = alpha.toUpperCase()
+
+    Computer output
+        var password = [ ]
+
+
+Launch button
+    on click launch function to start the generator 
+
+Function contains the below -
+
+prompt 
+    enter number of characters 8-128
+        parse int 
+        say not valid if NaN or if <8 or >128
+
+confirm
+    numbers yes/no    
+    lower case letters yes/no
+    upper case letters yes/no
+    special characters yes/no
+
+generate random selection of based on confirmed character types
+
+display in text area 
+
+*/
